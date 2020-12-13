ef77946fff5d9115748de43d6a16d87402776482
Richard-Netto <kenanrd@gmail.com>
2020-12-13 00:10:34 -0300

    Main base

---
+++ URS/URS.ino
@@ -0,0 +1,24 @@
+
+/**************************************************/
+/* File name:        URS.ino                      */
+/* File description: Main functon implementation  */
+/*                   for the URS project in the   */
+/*                   ESP32-CAM micro controller.  */
+/* Author name:      Richard Netto                */
+/* Creation date:    20/11/2020                   */
+/* Revision date:    20/11/2020                   */
+/**************************************************/
+
+// Library Includes
+
+// Variables
+
+// Setup Function
+void setup() {
+  
+}
+
+// Loop Function
+void loop() {
+  
+}
